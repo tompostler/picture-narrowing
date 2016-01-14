9c0967d586f5fa7f3836217d7a68232c6ec75ab1
Tom Postler <tom@postler.me>
2016-01-13 20:50:26 -0600

    Sizing limits

--- picture-narrowing/picture-narrowing/Form1.Designer.cs
+++ picture-narrowing/picture-narrowing/Form1.Designer.cs
@@ -35,10 +35,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(624, 441);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(640, 480);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Picture Narrowing";
             this.ResumeLayout(false);
 
         }
