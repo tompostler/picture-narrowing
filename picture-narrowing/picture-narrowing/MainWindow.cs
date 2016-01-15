11af62b3baea71ae7cab806879511d3d7ab98de2
Tom Postler <tom@postler.me>
2016-01-14 23:28:31 -0600

    Add the missing files...

---
+++ picture-narrowing/picture-narrowing/MainWindow.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picture_narrowing
+{
+    public partial class MainWindow : Form
+    {
+        private Manager manager;
+        private FileInfo image;
+        // TODO FIX
+        private string randomImageToReopenAtEnd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KeepButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(randomImageToReopenAtEnd))
+            {
+                randomImageToReopenAtEnd = Path.Combine(new string[] { image.DirectoryName, "keep", image.Name });
+            }
+            image.CopyTo(Path.Combine(new string[] { image.DirectoryName, "keep", image.Name }));
+            nextImage();
+        }
+
+        private void TossButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(randomImageToReopenAtEnd))
+            {
+                randomImageToReopenAtEnd = Path.Combine(new string[] { image.DirectoryName, "toss", image.Name });
+            }
+            image.CopyTo(Path.Combine(new string[] { image.DirectoryName, "toss", image.Name }));
+            nextImage();
+        }
+
+        private void SkipButton_Click(object sender, EventArgs e)
+        {
+            nextImage(false);
+        }
+
+        private void MainWindow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo directory = new DirectoryInfo(chooseDirectory());
+
+            // Create the necessary subdirectories if not already existing
+            try
+            {
+                directory.CreateSubdirectory("keep");
+            }
+            catch (IOException) { }
+            try
+            {
+                directory.CreateSubdirectory("toss");
+            }
+            catch (IOException) { }
+
+            manager = new Manager(directory);
+            nextImage(false);
+        }
+
+        private string chooseDirectory()
+        {
+            while (FolderDialog.ShowDialog() != DialogResult.OK) ;
+            return FolderDialog.SelectedPath;
+        }
+
+        private void nextImage(bool removeImage = true)
+        {
+            if (removeImage)
+            {
+                manager.RemoveImage(image);
+                updateRemainingLabel();
+            }
+
+            // Check for end
+            if (manager.ImagesRemaining == 0)
+            {
+                ImageViewer.Load(randomImageToReopenAtEnd);
+                Close();
+            }
+            else
+            {
+                // Show the next image
+                image = manager.RandomImage;
+                ImageViewer.Load(image.FullName);
+                updateRemainingLabel();
+            }
+        }
+
+        private void updateRemainingLabel()
+        {
+            RemainingImages.Text = $"{manager.ImagesRemaining - 1} Remaining...";
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            manager.DeleteFiles();
+        }
+    }
+}
---
+++ picture-narrowing/picture-narrowing/Manager.cs
@@ -0,0 +1,80 @@
+﻿namespace picture_narrowing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+
+    internal sealed class Manager
+    {
+        public DirectoryInfo Directory { get; private set; }
+        public int ImagesRemaining => _images.Count;
+        public FileInfo RandomImage => _images[_random.Next(_images.Count)];
+
+        private List<FileInfo> _images;
+        private List<FileInfo> _toDelete;
+        private Random _random;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="directory">The path to the directory with images in it.</param>
+        public Manager(string directory) : this(new DirectoryInfo(directory)) { }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="directory">The DirectoryInfo object for the directory containing the images.</param>
+        public Manager(DirectoryInfo directory)
+        {
+            Directory = directory;
+            _images = new List<FileInfo>();
+            _random = new Random();
+
+            _images = Directory.EnumerateFiles().ToList();
+            trimOutNonimages();
+        }
+
+        /// <summary>
+        /// Removes the image from the list of remaining images.
+        /// </summary>
+        /// <param name="image"></param>
+        public void RemoveImage(FileInfo image)
+        {
+            _images.Remove(image);
+            _toDelete.Add(image);
+        }
+
+        public void DeleteFiles()
+        {
+            foreach (FileInfo info in _toDelete)
+            {
+                info.Delete();
+            }
+        }
+
+        /// <summary>
+        /// Attempt to open each file using the Image class and only keep the ones that successfully
+        /// open as image files.
+        /// </summary>
+        private void trimOutNonimages()
+        {
+            List<FileInfo> actuallyImages = new List<FileInfo>();
+            foreach (FileInfo info in _images)
+            {
+                try
+                {
+                    Image.FromFile(info.FullName);
+                }
+                catch (OutOfMemoryException)
+                {
+                    continue;
+                }
+                actuallyImages.Add(info);
+            }
+            _images = actuallyImages;
+            _toDelete = new List<FileInfo>(_images.Count);
+        }
+    }
+}
